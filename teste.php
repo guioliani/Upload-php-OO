975ab3c50816b69e3e01fa67989fda92a333e27a
guilherme <guilhermeagutoli@gmail.com>
2018-05-07 23:49:18 -0300

    arquivo de testes

--- teste.php
+++ teste.php
@@ -10,9 +10,9 @@ use classes\dbFunctions;
 
 $query = new DbFunctions();
 $query->conecta();
-//$query->insert("INSERT INTO cliente (id, nome, email, telefone) VALUES (3, 'maria', 'maria@email.com', 5215151)");
-//$query->delete("DELETE FROM cliente WHERE id = 15454");
-//$query->update("UPDATE cliente SET nome = 'guilherme' WHERE id = 0");
+/*$query->insert("INSERT INTO cliente (id, nome, email, telefone) VALUES (3, 'maria', 'maria@email.com', 5215151)");
+$query->delete("DELETE FROM cliente WHERE id = 15454");
+$query->update("UPDATE cliente SET nome = 'guilherme' WHERE id = 0");*/
 $query->select("SELECT * FROM usuarios");
 
 ?>
