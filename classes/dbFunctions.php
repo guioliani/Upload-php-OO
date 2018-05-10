ec6e0cb48e3a68f4b4f1a26237dd2dc5c3ff5f45
guilherme <guilhermeagutoli@gmail.com>
2018-05-09 23:40:55 -0300

    Implementado metodo de login e cadastro

--- classes/dbFunctions.php
+++ classes/dbFunctions.php
@@ -10,13 +10,17 @@ class DbFunctions implements DB{
     private $host = "localhost";
     private $user = "root";
     private $pass = "";
-    private $db = "ex1";
+    private $db = "documentos";
+    private $result;
 
     private function getDbType(){return $this->dbtype;}
     private function getHost(){return $this->host;}
     private function getUser(){return $this->user;}
     private function getPass(){return $this->pass;}
     private function getDb() {return $this->db;}
+
+    private function getResult(){return $this->result;}
+    private function setResult($r){$this->result = $r;}
 
     /*metodo de conexao*/
     public function conecta(){
@@ -32,8 +36,11 @@ class DbFunctions implements DB{
     public function select($sql){
         $query = $this->conecta()->query($sql);
         foreach($query as $row){
-            print $row["nome"] . "<br/>";
+            //print $row["email"] . "<br/>";
+            $this->setResult($row);
+            return true;
         }
+        //$this->setResult($query->fetch(\PDO::FETCH_OBJ));
         $result = $query->fetch(\PDO::FETCH_OBJ);
     }
 
@@ -64,6 +71,64 @@ class DbFunctions implements DB{
             $query->execute(array($sql));
         }catch(PDOException $e){
             die("ERROR: <code>".$e->getMessage()."</code>");
+        }
+    }
+
+    /*metodo de login*/
+    public function login($email, $senha){
+        $query = $this->select("SELECT id, email, senha, nivel, status FROM usuarios WHERE email = '$email' AND senha = '$senha'
+                                    UNION
+                                SELECT id, email, senha, nivel, status FROM administrador WHERE email = '$email' AND senha = '$senha'");
+
+        if(count($this->getResult()) > 1){
+            $nivel = $this->getResult()['nivel'];
+            $status = $this->getResult()['status'];
+            if($nivel == 1 AND $status == 1){
+                echo "
+                <div class='aviso-cad green'>
+                    <h2 class='font-media titulo'>Logado com sucesso!!</h2>
+                </div>
+                ";
+            }elseif($nivel == 2 AND $status == 2){
+                echo "
+                <div class='aviso-cad green'>
+                    <h2 class='font-media titulo'>Logado com sucesso!!</h2>
+                </div>
+                 ";
+            }else{
+                echo "<div class='aviso-cad yellow'>
+                    <h2 class='font-media titulo'>Sua conta foi bloqueada ou ainda nao foi liberada!!</h2>
+                </div>";
+            }
+         
+        }else{
+            echo "<div class='aviso-cad red'>
+                <h2 class='font-media titulo'>Usuario ou senha incorretos!!</h2>
+            </div>";
+        }
+    }
+
+    /*metodo cadastrar*/
+    public function cadastrar($nome, $email, $senha){
+        $query = $this->select("SELECT * FROM usuarios WHERE email = '$email'");
+        if(count($this->getResult()) >= 1){
+            echo "
+                <div class='aviso-cad red'>
+                    <h2 class='font-media titulo'>Ja existe um usuario registrado com esse email!!</h2>
+                </div>";
+        }else{
+            $primeiroNome = explode(" ", $nome);
+            $caminho = 'arquivo/'.$primeiroNome[0];
+            $query = $this->insert("INSERT INTO `usuarios` (`nome`, `pasta`, `email`, `senha`, `status`, `nivel`, `capacidade`) VALUES ('$nome', '$primeiroNome[0]', '$email', '".md5($senha)."', 0, 1, 5368709120)");
+            if($query){
+                echo "<div class='aviso-cad green'>
+                    <h2 class='font-media titulo'>Conta registrada com sucesso!!</h2>
+                    </div>";
+            }else{
+                echo "<div class='aviso-cad red'>
+                    <h2 class='font-media titulo'>Erro ao se registrar, contate um administrador!!</h2>
+                    </div>";
+            }
         }
     }
 }
